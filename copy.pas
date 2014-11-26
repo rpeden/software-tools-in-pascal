98dba07adf120c5b301f29851fd9b76ac753a2d2
Ryan Peden <ryan04@gmail.com>
2014-11-25 20:53:44 -0500

    added compiler directive in copy program to include getc file

--- copy.pas
+++ copy.pas
@@ -7,23 +7,7 @@ const
 type
 	character = -1..127;  { ASCII, plus ENDFILE }
 
-{ getc -- get one character from standard input }
-function getc (var c : character) : character;
-var
-	ch : char;
-begin
-	if (eof) then 
-		c := ENDFILE
-	else if (eoln) then begin
-		readln;
-		c := NEWLINE
-	end 
-	else begin 
-		read(ch);
-		c := ord(ch)
-	end;
-	getc := c
-end;
+{$INCLUDE getc.pas}
 
 { putc -- put one character on standard input }
 procedure putc (c : character);
