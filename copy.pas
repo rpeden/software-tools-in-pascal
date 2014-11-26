3621cdde79472ed7b22e978e774c8441695fddba
Ryan Peden <ryan04@gmail.com>
2014-11-25 20:36:52 -0500

    added putc procedure

--- copy.pas
+++ copy.pas
@@ -24,3 +24,12 @@ begin
 	end;
 	getc := c
 end;
+
+{ putc -- put one character on standard input }
+procedure putc (c : character);
+begin 
+	if(c = NEWLINE) then
+		writeln
+	else
+		write(chr(c))
+end
