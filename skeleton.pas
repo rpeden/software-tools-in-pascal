9a8d47ea4b85cb67bbf9cf08f89a43c2224de481
Ryan Peden <ryan04@gmail.com>
2014-11-26 08:20:10 -0500

    added skeleton program to serve as starting point for future exercises from the book

---
+++ skeleton.pas
@@ -0,0 +1,16 @@
+program someprogram(input, output);
+
+const 
+	ENDFILE = -1;
+	NEWLINE = 10; { ASCII  value }
+	BLANK   = 32;
+	TAB     = 9;
+type
+	character = -1..127;  { ASCII, plus ENDFILE }
+
+{$INCLUDE getc.pas}
+{$INCLUDE putc.pas}
+
+begin
+	{run something here}
+end.
