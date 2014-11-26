4fd9037ba141fb020dcde4c9f3538b7a39480d16
Ryan Peden <ryan04@gmail.com>
2014-11-25 20:32:32 -0500

    added getc function

--- copy.pas
+++ copy.pas
@@ -6,3 +6,21 @@ const
 	NEWLINE = 10; { ASCII  value }
 type
 	character = -1..127;  { ASCII, plus ENDFILE }
+
+{ getc -- get one character from standard input }
+function getc (var c : character) : character;
+var
+	ch : char;
+begin
+	if (eof) then 
+		c := ENDFILE
+	else if (eoln) then begin
+		readln;
+		c := NEWLINE
+	end 
+	else begin 
+		read(ch);
+		c := ord(ch)
+	end;
+	getc := c
+end;
