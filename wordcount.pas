80154766106aa1aa163fe7a3c46c4729ca4777ec
Ryan Peden <ryan04@gmail.com>
2014-11-26 08:18:36 -0500

    added blank and tab constants to wordcount program

--- wordcount.pas
+++ wordcount.pas
@@ -3,6 +3,8 @@ program wordcountprogram(input, output);
 const 
 	ENDFILE = -1;
 	NEWLINE = 10; { ASCII  value }
+	BLANK   = 32;
+	TAB     = 9;
 type
 	character = -1..127;  { ASCII, plus ENDFILE }
 
